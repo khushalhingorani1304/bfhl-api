97475c1715072dcb25dfd4ea61d564e5c7a160b0
Khushal Hingorani <khushalhingorani1304@gmail.com>
2025-07-29 20:17:35 +0530

    feat: added /bfhl route with static response

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ app.use(express.json())
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
+
+app.post("/bfhl", (req, res) => {
+  res.status(200).json({
+    is_success: true,
+    user_id: "khushal_hingorani_1304",
+    email: "khushal3802.be22@chitkara.edu.in",
+    roll_number: "2210993802",
+    odd_numbers: [],
+    even_numbers: [],
+    alphabets: [],
+    special_characters: [],
+    sum: "0",
+    concat_string: ""
+  })
+})
